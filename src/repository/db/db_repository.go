0705a348fcee36fbc5c518f4c483f8bb5e92134d
Mustafa Kocatepe <msaidkocatepe@gmail.com>
2022-05-04 00:50:53 +0300

    --comm

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -6,6 +6,10 @@ import (
 	"github.com/mustafakocatepe/go-bookstore-oauth-api/src/utils/errors"
 )
 
+const (
+	queryGetAccessToken = "SELECT access_token, user_id, client_id, expires FROM access_tokens WHERE access_token=?;"
+)
+
 type DbRepository interface {
 	GetById(string) (*access_token.AccessToken, *errors.RestErr)
 }
@@ -21,5 +25,15 @@ func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.Re
 	session := cassandra.GetSession()
 	defer session.Close()
 
-	return nil, errors.NewInternalServerError("database connection not implemented yet")
+	var result access_token.AccessToken
+	if err := session.Query(queryGetAccessToken, id).Scan(
+		&result.AccessToken,
+		&result.UserId,
+		&result.ClientId,
+		&result.Expires,
+	); err != nil {
+		return nil, errors.NewInternalServerError(err.Error())
+	}
+
+	return &result, nil
 }
